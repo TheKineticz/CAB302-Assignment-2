96c14aa1305e674eefea9397aeddd2783c77e8ed
Joel Bartosch <50476983+TheKineticz@users.noreply.github.com>
2019-05-29 22:39:01 +1000

    Implement PlotCommand

---
+++ src/main/java/com/thekineticz/vectool/vec/commands/PlotCommand.java
@@ -0,0 +1,75 @@
+package com.thekineticz.vectool.vec.commands;
+
+import com.thekineticz.vectool.exception.VecCommandException;
+
+/**
+ * The internal structure of a plot vec command.
+ */
+public class PlotCommand extends VecCommand {
+
+    private static final String COMMAND_NAME = "PLOT";
+    private static final Integer REQUIRED_POSITION_VALUES = 2;
+
+    private Position<Double> position;
+
+    /**
+     * Constructs a new PlotCommand.
+     *
+     * @param position The position of the plot.
+     */
+    public PlotCommand(Position<Double> position){
+        super(COMMAND_NAME);
+        this.position = position;
+    }
+
+    /**
+     * Constructs a PlotCommand object from a plot command string.
+     *
+     * @param command The command string.
+     * @return The PlotCommand object representing the command string.
+     * @throws VecCommandException Thrown if an error occurs while parsing the string.
+     */
+    public static PlotCommand fromString(String command) throws VecCommandException{
+        String[] commandArray = command.split(" ");
+
+        if (commandArray.length - 1 != REQUIRED_POSITION_VALUES){
+            throw new VecCommandException(String.format("%s command string must contain %d arguments, separated by a space.", COMMAND_NAME, REQUIRED_POSITION_VALUES + 1));
+        }
+
+        if (!commandArray[0].equals(COMMAND_NAME)){
+            throw new VecCommandException("Attempted to generate PlotCommand from string with incorrect identifier.");
+        }
+
+        Double x;
+        Double y;
+
+        try{
+            x = Double.valueOf(commandArray[1]);
+            y = Double.valueOf(commandArray[2]);
+        }
+        catch (Exception e){
+            throw new VecCommandException(String.format("Attempted to parse invalid argument for %s command position.", COMMAND_NAME), e);
+        }
+
+
+        return new PlotCommand(new Position<>(x, y));
+    }
+
+    /**
+     * Get the arguments of the plot command.
+     *
+     * @return The position of the plot in string form.
+     */
+    public String getArgs(){
+        return position.toString();
+    }
+
+    /**
+     * Get the position of the plot.
+     *
+     * @return The position of the plot.
+     */
+    public Position getPositions(){
+        return position;
+    }
+}

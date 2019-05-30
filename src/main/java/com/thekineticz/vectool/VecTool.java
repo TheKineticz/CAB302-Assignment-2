85301db022c8f1036d3ae747f0725835692f2321
Joel Bartosch <50476983+TheKineticz@users.noreply.github.com>
2019-05-30 22:36:18 +1000

    Begin GUI implementation

--- src/main/java/com/thekineticz/vectool/VecTool.java
+++ src/main/java/com/thekineticz/vectool/VecTool.java
@@ -1,7 +1,26 @@
 package com.thekineticz.vectool;
 
+/**
+ * Instance manager for the VecTool application.
+ */
 public class VecTool {
-    public static void main(String[] args) {
 
+    private static VecTool applicationInstance;
+
+    /**
+     * Registers a new VecTool application instance as running.
+     *
+     * @throws IllegalStateException Thrown if an instance of the VecTool application is already running.
+     */
+    public VecTool(){
+        if (applicationInstance != null){
+            throw new IllegalStateException("Application is already running.");
+        }
+
+        applicationInstance = this;
+    }
+
+    public void close(){
+        applicationInstance = null;
     }
 }
--- src/main/java/com/thekineticz/vectool/gui/VecToolGui.java
+++ src/main/java/com/thekineticz/vectool/gui/VecToolGui.java
@@ -1,4 +1,37 @@
 package com.thekineticz.vectool.gui;
 
+import com.thekineticz.vectool.VecTool;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * VecTool GUI container class and program entry point.
+ */
 public class VecToolGui {
+
+    private static final String TITLE = "VECtor Design Tool";
+    private JFrame frame;
+
+    public VecToolGui(){
+        new VecTool();
+
+        //Form setup
+        frame = new JFrame();
+        frame.setTitle(TITLE);
+
+        //Frame setup
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    /**
+     * The entry point for the GUI Application.
+     * 
+     * @param args String array arguments (Ignored)
+     */
+    public static void main(String[] args){
+        new VecToolGui();
+    }
 }

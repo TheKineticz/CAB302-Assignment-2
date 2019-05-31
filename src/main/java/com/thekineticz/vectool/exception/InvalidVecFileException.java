6b82f44361df2eeda031e1316c38e3a8696ab241
Joel Bartosch <50476983+TheKineticz@users.noreply.github.com>
2019-05-31 22:33:14 +1000

    Refactor VecIOException name to InvalidVecFileException

---
+++ src/main/java/com/thekineticz/vectool/exception/InvalidVecFileException.java
@@ -0,0 +1,8 @@
+package com.thekineticz.vectool.exception;
+
+public class InvalidVecFileException extends Exception{
+
+    public InvalidVecFileException(String message){
+        super(message);
+    }
+}
--- src/main/java/com/thekineticz/vectool/exception/VecIOException.java
+++
@@ -1,8 +0,0 @@
-package com.thekineticz.vectool.exception;
-
-public class VecIOException extends Exception{
-
-    public VecIOException(String message){
-        super(message);
-    }
-}

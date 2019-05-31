86006b8865ba8bb2c2f9cde53eb00ad3c702347e
Joel Bartosch <50476983+TheKineticz@users.noreply.github.com>
2019-05-31 17:16:46 +1000

    Create beginning of GUI

---
+++ src/main/java/com/thekineticz/vectool/gui/VecToolGUI.java
@@ -0,0 +1,41 @@
+package com.thekineticz.vectool.gui;
+
+import com.thekineticz.vectool.VecTool;
+import com.thekineticz.vectool.vec.VecFile;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * VecTool GUI container class and program entry point.
+ */
+public class VecToolGUI {
+
+    private static final String TITLE = "VECtor Design Tool";
+    private VecFile vecFile = null;
+
+    private JFrame frame;
+
+    /**
+     * Creates and shows the VecTool GUI.
+     * For safety, this should be invoked from the event-dispatching thread.
+     */
+    public VecToolGUI() {
+        new VecTool();
+
+        frame = new JFrame();
+        frame.setTitle(TITLE);
+
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+    }
+
+    /**
+     * The entry point for the GUI Application.
+     *
+     * @param args String array arguments (Ignored)
+     */
+    public static void main(String[] args){
+        javax.swing.SwingUtilities.invokeLater(VecToolGUI::new);
+    }
+}

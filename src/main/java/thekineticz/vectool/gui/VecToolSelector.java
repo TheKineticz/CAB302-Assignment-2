eeee3938d10b22171e5a8c38aaa218fd57bb3eb0
Joel Bartosch <50476983+TheKineticz@users.noreply.github.com>
2019-06-02 19:40:45 +1000

    Improve tooltips

--- src/main/java/thekineticz/vectool/gui/VecToolSelector.java
+++ src/main/java/thekineticz/vectool/gui/VecToolSelector.java
@@ -28,35 +28,35 @@ class VecToolSelector extends JToolBar {
         //Set up plot button
         plotToolButton = new JToggleButton(new ImageIcon(getClass().getResource("plot.png")));
         plotToolButton.setFocusPainted(false);
-        plotToolButton.setToolTipText("Plot");
+        plotToolButton.setToolTipText("<html><b>Plot</b><p>Freeform drawing tool.</p></html>");
         tools.add(plotToolButton);
         add(plotToolButton, constraints);
 
         //Set up line button
         lineToolButton = new JToggleButton(new ImageIcon(getClass().getResource("line.png")));
         lineToolButton.setFocusPainted(false);
-        lineToolButton.setToolTipText("Line");
+        lineToolButton.setToolTipText("<html><b>Line</b><p>Click and drag to create a line.</p></html>");
         tools.add(lineToolButton);
         add(lineToolButton, constraints);
 
         //Set up rectangle button
         rectangleToolButton = new JToggleButton(new ImageIcon(getClass().getResource("rectangle.png")));
         rectangleToolButton.setFocusPainted(false);
-        rectangleToolButton.setToolTipText("Rectangle");
+        rectangleToolButton.setToolTipText("<html><b>Rectangle</b><p>Click and drag to create a rectangle.</p></html>");
         tools.add(rectangleToolButton);
         add(rectangleToolButton, constraints);
 
         //Set up ellipse button
         ellipseToolButton = new JToggleButton(new ImageIcon(getClass().getResource("ellipse.png")));
         ellipseToolButton.setFocusPainted(false);
-        ellipseToolButton.setToolTipText("Ellipse");
+        ellipseToolButton.setToolTipText("<html><b>Ellipse</b><p>Click and drag to create an ellipse.</p></html>");
         tools.add(ellipseToolButton);
         add(ellipseToolButton, constraints);
 
         //Set up polygon button
         polygonToolButton = new JToggleButton(new ImageIcon(getClass().getResource("polygon.png")));
         polygonToolButton.setFocusPainted(false);
-        polygonToolButton.setToolTipText("Polygon");
+        polygonToolButton.setToolTipText("<html><b>Polygon</b><p>Click to create a vertex. Snaps to end.</p></html>");
         tools.add(polygonToolButton);
         add(polygonToolButton, constraints);
     }
